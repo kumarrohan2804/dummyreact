1bd1b2279e69804a1bf58d25ad2620d356649559
kumarrohan2804 <kumarrohan2804@gmail.com>
2021-07-19 13:45:37 +0530

    header component styling

---
+++ src/Components/Layout/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+import mealsImage from "../../Assets/meals.jpg";
+import classes from "./Header.module.css";
+import HeaderCartButton from "./HeaderCartButton";
+
+const Header = () => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>React Meals</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="table of foods" />
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default Header;
